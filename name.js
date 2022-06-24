9c4da8c1c2da897349b398b553ee3c0f3b970f57
Efunsanwo01 <jdayo3rd@gmail.com>
2022-06-24 01:11:55 +0100

    first commit

---
+++ name.js
@@ -0,0 +1,9 @@
+const firstname = 'Temidayo';
+const lastname = 'Efunsanwo';
+var heigt = '6.2';
+let country = 'Nigeria';
+
+console.log (firstname);
+console.log (lastname);
+console.log (heigt);
+console.log (country);
